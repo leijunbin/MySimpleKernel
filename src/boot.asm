c972653a85c8ccf74ee124e8ec9ff40abb496dec
barars <1286561513@qq.com>
2022-02-13 02:06:42 -0800

    read and write disk

--- src/boot.asm
+++ src/boot.asm
@@ -1,19 +1,189 @@
 [org 0x7c00]
 
+; 清空屏幕
 mov ax, 3
 int 0x10
 
+; 初始化寄存器
 mov ax, 0
 mov ds, ax
 mov es, ax
 mov ss, ax
 mov sp, 0x7c00
 
-mov ax, 0xb800
-mov ds, ax
-mov byte [0], 'H'
+mov si, booting
+call print
+
+mov edi, 0x1000 ;目标内存
+mov ecx, 0 ;起始扇区编号
+mov bl, 1 ;扇区数量
+
+call read_disk
+
+mov edi, 0x1000 ;目标内存
+mov ecx, 2 ;起始扇区编号
+mov bl, 1 ;扇区数量
+
+call write_disk
+
+xchg bx, bx
 
 jmp $
+
+read_disk:
+    ;设置读写扇区数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx ;0x1f3
+    mov al, cl ;起始扇区编号低8位
+    out dx, al
+
+    inc dx ;0x1f4
+    shr ecx, 8
+    mov al, cl ;起始扇区编号中8位
+    out dx, al
+    
+    inc dx ;0x1f5
+    shr ecx, 8
+    mov al, cl ;起始扇区编号高8位
+    out dx, al
+    
+    inc dx ;0x1f6
+    shr ecx, 8
+    and cl, 0b1111 ;将高四位设置为0
+    
+    mov al, 0b1110_0000 ;主盘，LBA模式
+    or al, cl
+    out dx, al
+
+    inc dx ;0x1f7
+    mov al, 0x20 ;读硬盘
+    out dx, al
+
+    xor ecx, ecx
+    mov cl, bl ;得到读写扇区数量
+
+    .read:
+        push cx ;保存cx
+        call .waits ;等待数据准备完毕
+        call .reads ;读取一个扇区
+        pop cx
+        loop .read
+
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check
+            in al, dx
+            jmp $+2 ;直接跳转下一行
+            jmp $+2 ;一点点延迟
+            jmp $+2
+            and al, 0b1000_1000
+            cmp al, 0b0000_1000
+            jnz .check
+        ret
+
+    .reads:
+        mov dx, 0x1f0
+        mov cx, 256
+        .readw:
+            in ax, dx
+            jmp $+2 ;直接跳转下一行
+            jmp $+2 ;一点点延迟
+            jmp $+2
+            mov [edi], ax
+            add edi, 2
+            loop .readw
+        ret 
+
+
+write_disk:
+    ;设置读写扇区数量
+    mov dx, 0x1f2
+    mov al, bl
+    out dx, al
+
+    inc dx ;0x1f3
+    mov al, cl ;起始扇区编号低8位
+    out dx, al
+
+    inc dx ;0x1f4
+    shr ecx, 8
+    mov al, cl ;起始扇区编号中8位
+    out dx, al
+    
+    inc dx ;0x1f5
+    shr ecx, 8
+    mov al, cl ;起始扇区编号高8位
+    out dx, al
+    
+    inc dx ;0x1f6
+    shr ecx, 8
+    and cl, 0b1111 ;将高四位设置为0
+    
+    mov al, 0b1110_0000 ;主盘，LBA模式
+    or al, cl
+    out dx, al
+
+    inc dx ;0x1f7
+    mov al, 0x30 ;写硬盘
+    out dx, al
+
+    xor ecx, ecx
+    mov cl, bl ;得到读写扇区数量
+
+    .write:
+        push cx ;保存cx
+        call .writes ;写一个扇区
+        call .waits ;等待数据写完，结束繁忙
+        pop cx
+        loop .write
+
+    ret
+
+    .waits:
+        mov dx, 0x1f7
+        .check
+            in al, dx
+            jmp $+2 ;直接跳转下一行
+            jmp $+2 ;一点点延迟
+            jmp $+2
+            and al, 0b1000_0000
+            cmp al, 0b0000_0000
+            jnz .check
+        ret
+
+    .writes:
+        mov dx, 0x1f0
+        mov cx, 256
+        .writew:
+            mov ax, [edi]
+            out dx, ax
+            jmp $+2 ;直接跳转下一行
+            jmp $+2 ;一点点延迟
+            jmp $+2
+            mov [edi], ax
+            add edi, 2
+            loop .writew
+        ret 
+
+print:
+    mov ah, 0x0e
+    .next:
+        mov al, [si]
+        cmp al, 0
+        jz .done
+        int 0x10
+        inc si
+        jmp .next
+    .done
+        ret
+
+booting:
+    db "Booting Myos...", 10, 13, 0 ;\n\r\0
 
 times 510 - ($ - $$) db 0
 
