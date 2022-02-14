611cc73704a861eb8e47c68fce02d5d06cbc2d72
barars <1286561513@qq.com>
2022-02-14 03:12:38 -0800

    gdt and detecte memory

--- src/boot.asm
+++ src/boot.asm
@@ -15,20 +15,15 @@ mov si, booting
 call print
 
 mov edi, 0x1000 ;目标内存
-mov ecx, 0 ;起始扇区编号
-mov bl, 1 ;扇区数量
+mov ecx, 2 ;起始扇区编号
+mov bl, 4 ;扇区数量
 
 call read_disk
 
-mov edi, 0x1000 ;目标内存
-mov ecx, 2 ;起始扇区编号
-mov bl, 1 ;扇区数量
+cmp word [0x1000], 0x55aa
+jnz error
 
-call write_disk
-
-xchg bx, bx
-
-jmp $
+jmp 0:0x1002
 
 read_disk:
     ;设置读写扇区数量
@@ -100,75 +95,13 @@ read_disk:
         ret 
 
 
-write_disk:
-    ;设置读写扇区数量
-    mov dx, 0x1f2
-    mov al, bl
-    out dx, al
-
-    inc dx ;0x1f3
-    mov al, cl ;起始扇区编号低8位
-    out dx, al
-
-    inc dx ;0x1f4
-    shr ecx, 8
-    mov al, cl ;起始扇区编号中8位
-    out dx, al
-    
-    inc dx ;0x1f5
-    shr ecx, 8
-    mov al, cl ;起始扇区编号高8位
-    out dx, al
-    
-    inc dx ;0x1f6
-    shr ecx, 8
-    and cl, 0b1111 ;将高四位设置为0
-    
-    mov al, 0b1110_0000 ;主盘，LBA模式
-    or al, cl
-    out dx, al
-
-    inc dx ;0x1f7
-    mov al, 0x30 ;写硬盘
-    out dx, al
-
-    xor ecx, ecx
-    mov cl, bl ;得到读写扇区数量
-
-    .write:
-        push cx ;保存cx
-        call .writes ;写一个扇区
-        call .waits ;等待数据写完，结束繁忙
-        pop cx
-        loop .write
-
-    ret
-
-    .waits:
-        mov dx, 0x1f7
-        .check
-            in al, dx
-            jmp $+2 ;直接跳转下一行
-            jmp $+2 ;一点点延迟
-            jmp $+2
-            and al, 0b1000_0000
-            cmp al, 0b0000_0000
-            jnz .check
-        ret
-
-    .writes:
-        mov dx, 0x1f0
-        mov cx, 256
-        .writew:
-            mov ax, [edi]
-            out dx, ax
-            jmp $+2 ;直接跳转下一行
-            jmp $+2 ;一点点延迟
-            jmp $+2
-            mov [edi], ax
-            add edi, 2
-            loop .writew
-        ret 
+error:
+    mov si, .msg
+    call print
+    hlt ;CPU停机
+    jmp $
+    .msg:
+        db "Booting Error!!!", 10, 13, 0
 
 print:
     mov ah, 0x0e
